4d7db7f785109d0ab45328328cd41f198a95f9a3
D3C0D1NG <joseph.s.foster@icloud.com>
2023-10-29 13:25:27 -0500

    added styling to icons and enhanced js

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const powerButton = document.getElementById("powerButton");
-    const header = document.querySelector(".hidden-header");
-    const container = document.querySelector(".container");
-    const main = document.querySelector(".main");
-    const footer = document.querySelector(".footer");
-  
-    if (powerButton) {
-      powerButton.addEventListener("click", () => {
-        main.classList.remove("main");
-        header.classList.remove("hidden-header");
-        container.style.display = "none";
-        footer.style.position = "relative";
-      });
-    }
-  });
-  
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const powerButton = document.getElementById("powerButton");
+  const header = document.querySelector(".hidden-header");
+  const container = document.querySelector(".container");
+  const main = document.querySelector(".main");
+  const footer = document.querySelector(".footer");
+
+  if (powerButton) {
+    powerButton.addEventListener("click", () => {
+      main.classList.remove("main");
+      header.classList.remove("hidden-header");
+      container.style.display = "none";
+      footer.style.position = "relative";
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const reloadLink = document.getElementById("reloadLink");
+
+  if (reloadLink) {
+    reloadLink.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the default link behavior
+      window.location.reload(); // Reload the page
+    });
+  }
+});
