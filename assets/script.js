a829ba455c22bcca13e37541aa488cc0b55a3bfe
D3C0D1NG <joseph.s.foster@icloud.com>
2023-10-29 13:03:43 -0500

    added js to handle power button click

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector(".hidden-header");
     const container = document.querySelector(".container");
     const main = document.querySelector(".main");
-    // const footer = document.querySelector(".footer");
+    const footer = document.querySelector(".footer");
   
     if (powerButton) {
       powerButton.addEventListener("click", () => {
         main.classList.remove("main");
         header.classList.remove("hidden-header");
         container.style.display = "none";
+        footer.style.position = "relative";
       });
     }
   });
