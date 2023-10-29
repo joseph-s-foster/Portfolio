785f0cd6aafc103067159cbf64b1634570714950
D3C0D1NG <joseph.s.foster@icloud.com>
2023-10-29 16:21:05 -0500

    fixed bugs with page load

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const powerButton = document.getElementById("powerButton");
   const header = document.querySelector(".hidden-header");
   const container = document.querySelector(".container");
-  const main = document.querySelector(".main");
+  const main = document.querySelector(".hidden-main");
   const footer = document.querySelector(".footer");
 
   if (powerButton) {
     powerButton.addEventListener("click", () => {
-      main.classList.remove("main");
-      main.classList.add("fade-in");
+      main.classList.remove("hidden-main");
       header.classList.remove("hidden-header");
       container.style.display = "none";
       footer.style.position = "relative";
