f11dee3bec539f5bbda76ad1909bf0dbafa0620c
D3C0D1NG <joseph.s.foster@icloud.com>
2023-11-06 20:49:08 -0600

    removed powerbutton startup

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const powerButton = document.getElementById("powerButton");
-  const header = document.querySelector(".hidden-header");
-  const container = document.querySelector(".container");
-  const main = document.querySelector(".hidden-main");
-  const footer = document.querySelector(".footer");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const powerButton = document.getElementById("powerButton");
+//   const header = document.querySelector(".hidden-header");
+//   const container = document.querySelector(".container");
+//   const main = document.querySelector(".hidden-main");
+//   const footer = document.querySelector(".footer");
 
-  if (powerButton) {
-    powerButton.addEventListener("click", () => {
-      main.classList.remove("hidden-main");
-      header.classList.remove("hidden-header");
-      container.style.display = "none";
-      footer.style.position = "relative";
-      document.body.style.overflow = "auto";
-    });
-  }
-});
+//   if (powerButton) {
+//     powerButton.addEventListener("click", () => {
+//       main.classList.remove("hidden-main");
+//       header.classList.remove("hidden-header");
+//       container.style.display = "none";
+//       footer.style.position = "relative";
+//       document.body.style.overflow = "auto";
+//     });
+//   }
+// });
